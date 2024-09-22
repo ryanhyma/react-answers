a9fa1d2175ccb1f5330a3d0712561b65d178c018
Lisa Fast <lisa@vation.ca>
2024-09-22 18:18:48 -0400

    start again

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole('button', { name: /click me/i });
+  expect(buttonElement).toBeInTheDocument();
 });
