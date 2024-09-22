be17b261c1a5b01bb8808b08ae229ab7421d8382
Lisa Fast <lisa@vation.ca>
2024-09-22 12:40:15 -0400

    get started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GcdsContainer} from '@cdssnc/gcds-components-react';
+import { GcdsHeader} from '@cdssnc/gcds-components-react';
+import '@cdssnc/gcds-components-react/gcds.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <GcdsHeader
+  langHref="#"
+  skipToHref="#"
+>
+<div slot="breadcrumb"><a href="https://www.canada.ca/en.html">Canada.ca</a></div>
+</GcdsHeader>
+   
       </header>
+      <main>
+      <h1>AI Answers
+
+      </h1>
+      <GcdsContainer size="md" border padding="400">
+  <p>This is a responsive container, you can replace this text with any content or other components.</p>
+
+        </GcdsContainer>
+      </main>
     </div>
   );
 }
 
 export default App;
+
