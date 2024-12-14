c6eb508509677d6174686ad4b2cef9bbb882764b
Ryan Hyma <ryan.hyma@gmail.com>
2024-12-14 12:54:11 -0500

    Fix deploy

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
-// babel.config.js
 export default {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-private-property-in-object',
+    '@babel/plugin-transform-runtime'
+  ]
 };
