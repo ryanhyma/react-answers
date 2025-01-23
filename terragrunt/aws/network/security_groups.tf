b8e0c410869cc4e36701d84d1d4dd3147f73f0c4
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-01-23 23:00:53 +0000

    Formatting

--- terragrunt/aws/network/security_groups.tf
+++ terragrunt/aws/network/security_groups.tf
@@ -11,13 +11,13 @@ locals {
 # Create a Security Group for the React Answers App
 resource "aws_security_group" "app" {
   # Use the SG name defined in the local variable
-  name        = local.sg_app_name
+  name = local.sg_app_name
 
   # Short description for clarity in the AWS console
   description = "Security Group for the React Answers App"
 
   # ID of the VPC where this SG will be created
-  vpc_id      = module.react_answers_vpc.vpc_id
+  vpc_id = module.react_answers_vpc.vpc_id
 
   # Ensures Terraform removes associated rules upon SG destruction or modification
   revoke_rules_on_delete = true
@@ -32,18 +32,18 @@ resource "aws_security_group" "app" {
 # Create an egress rule to allow HTTPS traffic to the internet
 resource "aws_security_group_rule" "app_egress_https" {
   # Brief description to identify the purpose of this rule
-  description       = "Allow HTTPS (TCP 443) egress to the internet"
+  description = "Allow HTTPS (TCP 443) egress to the internet"
 
   # Egress rule allows outbound traffic
-  type              = "egress"
+  type = "egress"
 
   # The TCP port range (443 to 443) for HTTPS
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
 
   # Public internet CIDR block
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   # Attach the rule to the SG created above
   security_group_id = aws_security_group.app.id
