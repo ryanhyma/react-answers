0d50194bcfad9fbceb3dc2d9aac1856081eed4e8
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-01-23 22:20:35 +0000

    Adding code for VPC

---
+++ terragrunt/aws/network/vpc.tf
@@ -0,0 +1,26 @@
+# 
+# VPC module for React answers
+# 
+
+# Use the terraform-modules/vpc module to create the VPC for the react answers app
+module "react_answers_vpc" {
+    source = "github.com/cds-snc/terraform-modules//vpc?ref=v10.2.2"
+    name = var.product_name
+
+    # Deploy high availabiltiy mode 
+    high_availability = true
+
+    # Enables VPC flow logs and blocks ssh and rdp traffic
+    enable_flow_log   = true
+    block_ssh         = true
+    block_rdp         = true
+
+    # uses single nat gateway if not in production 
+    single_nat_gateway = var.env != "production"
+
+    # allow HTTPS connections on part 443 in/out from the internet and allow a repsonse back to/from the internet
+    allow_https_request_out          = true
+    allow_https_request_out_response = true
+    allow_https_request_in           = true
+    allow_https_request_in_response  = true
+}
