00c6c170f7a3c5e97af8f8c915fbb78ab3f828b8
Lisa Fast <lisa@vation.ca>
2025-02-04 17:36:33 -0500

    Update ircc-updates.js
    add extend or change conditions on work permit - weird that it didn't find it

--- src/services/systemPrompt/context-ircc/ircc-updates.js
+++ src/services/systemPrompt/context-ircc/ircc-updates.js
@@ -7,6 +7,8 @@ export const IRCC_UPDATES = `
 
 * updated Oct 2024 https://www.canada.ca/en/immigration-refugees-citizenship/services/lebanon-2023.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/liban-2023.html
 
+* updated Oct 2024 Extend or change the conditions on your work permit https://www.canada.ca/en/immigration-refugees-citizenship/services/work-canada/permit/temporary/extend.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/travailler-canada/permis/temporaire/prolongez-modifiez.html
+
 * updated Jan 2025 https://www.canada.ca/en/immigration-refugees-citizenship/services/israel-west-bank-gaza-2023.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/israel-cisjordanie-gaza-2023.html
 
 * As of January 21, 2025, at 5:00 UTC, the eligibility requirements will change for open work permits for family members of workers. https://www.canada.ca/en/immigration-refugees-citizenship/services/work-canada/permit/temporary/open-work-permit-spouses-dependent-children.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/travailler-canada/permis/temporaire/permis-travail-ouvert-epoux-enfant-charge.html
