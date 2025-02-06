ba28e111ff16109c3573f5a02fccf4d4cf1dc591
Lisa Fast <lisa@vation.ca>
2025-02-06 18:14:46 -0500

    Update isc-scenarios.js

--- src/services/systemPrompt/context-isc/isc-scenarios.js
+++ src/services/systemPrompt/context-isc/isc-scenarios.js
@@ -6,10 +6,14 @@ export const ISC_SCENARIOS = `
 * If the question isn't clear about processing time for registration vs status card, ask a clarifying question so you can provide a helpful answer.
 Processing time citations: https://www.sac-isc.gc.ca/eng/1710869258242/1710869294766 https://www.sac-isc.gc.ca/fra/1710869258242/1710869294766
 
-### Status cards - no online applications available
+### Status cards - no online applications or renewal available
 * People registered under the Indian Act can download the application form and submit it by mail or in person. Refer to the Where to submit your application page https://www.sac-isc.gc.ca/eng/1695839818435/1695839847447 https://www.sac-isc.gc.ca/fra/1695839818435/1695839847447
+* People can apply to renew their status card up to one year in advance. Eligibiility for simplified renewal depends on several factors on this page: https://www.sac-isc.gc.ca/eng/1695840367366/1695840394948 https://www.sac-isc.gc.ca/fra/1695840367366/1695840394948
 
 ### Jordan's Principle - no online applications available
 * How to submit a request via the Jordan's Principle Call Centre at 1-855-JP-CHILD (1-855-572-4453) or through a regional focal point https://www.sac-isc.gc.ca/eng/1568396296543/1582657596387 https://www.sac-isc.gc.ca/fra/1568396296543/1582657596387
 
+### Regional offices by province
+* Regional offices are listed by province, but the nearest office may be listed under a different province. For example, people in Ottawa Ontario are closest to the National Capital Region office in Gatineau Quebec. 
+* Some offices provide in-person and front counter service, some do not, some require pre-booking an appointment. Before providing office hours, in-person advice or telephone numbers in your response, use the "downloadWebPage" tool to read the relevant office page to provide an accurate answer.
 `;
