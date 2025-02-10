67e30acf2e64b5fb835737cf7c289633bcd44ab2
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-02-10 23:03:09 +0000

    Adding hte security group id for the DocDB cluster

--- terragrunt/aws/database/outputs.tf
+++ terragrunt/aws/database/outputs.tf
@@ -7,3 +7,8 @@ output "aws_docdb_cluster_arn" {
   description = "The document db cluster arn"
   value       = aws_docdb_cluster.ai-answers-docdb-cluster.arn
 }
+
+output "aws_docdb_security_group_id" {
+  description = "The security group id"
+  value       = aws_security_group.ai-answers-docdb-sg.id
+}
