cdfabb79f706be32f3658d57ba4724167ab154b9
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-02-17 10:36:15 -0500

    Update DocumentDB URI handling

--- terragrunt/aws/database/outputs.tf
+++ terragrunt/aws/database/outputs.tf
@@ -12,3 +12,14 @@ output "aws_docdb_security_group_id" {
   description = "The security group id of the document db database"
   value       = aws_security_group.ai-answers-docdb-sg.id
 }
+
+output "aws_docdb_cluster_endpoint" {
+  description = "The cluster endpoint"
+  value       = aws_docdb_cluster.ai-answers-docdb-cluster.endpoint
+}
+
+output "aws_docdb_connection_uri" {
+  description = "The connection URI for DocumentDB"
+  sensitive   = true
+  value       = "mongodb://${data.aws_ssm_parameter.docdb_username.value}:${data.aws_ssm_parameter.docdb_password.value}@${aws_docdb_cluster.ai-answers-docdb-cluster.endpoint}:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
