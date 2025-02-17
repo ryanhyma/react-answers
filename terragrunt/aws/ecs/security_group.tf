c0309304be37cabe72ad98992ea991228d02a881
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-02-17 09:04:44 -0500

    update rules to external

--- terragrunt/aws/ecs/security_group.tf
+++ terragrunt/aws/ecs/security_group.tf
@@ -7,26 +7,29 @@ resource "aws_security_group" "ecs_tasks" {
   description = "Allow inbound and outbound traffic for AI Answers"
   vpc_id      = var.vpc_id
 
-  ingress {
-    protocol        = "tcp"
-    description     = "Allow the ecs security group to receive traffic only from the load balancer on port 3001"
-    security_groups = ["${var.ai_answers_load_balancer_sg}"]
-    self            = "false"
-    from_port       = "3001"
-    to_port         = "3001"
-  }
-
-  egress {
-    description = "Allow ecs security group to send all traffic"
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     "CostCentre" = var.billing_code
   }
+}
+
+resource "aws_security_group_rule" "ecs_ingress_lb" {
+  description              = "Allow the ecs security group to receive traffic only from the load balancer on port 3001"
+  type                     = "ingress"
+  from_port                = 3001
+  to_port                  = 3001
+  protocol                 = "tcp"
+  source_security_group_id = var.ai_answers_load_balancer_sg
+  security_group_id        = aws_security_group.ecs_tasks.id
+}
+
+resource "aws_security_group_rule" "ecs_egress_all" {
+  description       = "Allow ecs security group to send all traffic"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs_tasks.id
 }
 
 ###
