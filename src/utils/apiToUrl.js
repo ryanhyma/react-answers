c372cbc89042b776268bdba1a8af3df18be0a96f
Ryan Hyma <ryan.hyma@gmail.com>
2025-03-06 15:46:21 -0500

    Update API URL construction to use window location in production

--- src/utils/apiToUrl.js
+++ src/utils/apiToUrl.js
@@ -1,9 +1,8 @@
 const getApiUrl = (endpoint) => {
   const baseUrl =
     process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_API_BASE_URL || '' // Use environment variable or fallback to relative paths
+      ? window.location.origin 
       : 'http://localhost:3001';
-
   return `${baseUrl}/api/${endpoint}`;
 };
 
@@ -16,9 +15,8 @@ const getProviderApiUrl = (provider, endpoint) => {
   }
   const baseUrl =
     process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_API_BASE_URL || '' // Use environment variable or fallback to relative paths
+      ? window.location.origin
       : 'http://localhost:3001';
-
   return `${baseUrl}/api/${provider}-${endpoint}`;
 };
 
