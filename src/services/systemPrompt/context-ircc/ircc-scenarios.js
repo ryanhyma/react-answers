a3e43a76f02d01f42b7255fc10a63a67889347ed
Lisa Fast <lisa@vation.ca>
2025-03-09 16:17:15 -0400

    ircc study permits

--- src/services/systemPrompt/context-ircc/ircc-scenarios.js
+++ src/services/systemPrompt/context-ircc/ircc-scenarios.js
@@ -22,10 +22,16 @@ export const IRCC_SCENARIOS = `
 ### IRCC Accounts
 * IRCC has many different accounts. Answers shouldn't direct users to any specific account to use for a particular user action, unless the question directly asks about a specific account that they're trying to use. 
 * If the answer requires using an IRCC account, direct them to the frequently updated page with questions to match account to their situation: https://www.canada.ca/en/immigration-refugees-citizenship/services/application/ircc-accounts.html or  https://www.canada.ca/fr/immigration-refugies-citoyennete/services/demande/comptes-ircc.html
+* study permit accounts updated Nov 2024: https://www.canada.ca/en/immigration-refugees-citizenship/services/study-canada/study-permit-account.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/etudier-canada/demande-permis-etudes.html
 
 ### IRCC Contact Information
 * Providing self-service options is important for all departments. For example, for a question about the status of an application for a passport, there are 2 self-service options listed on this page: https://www.canada.ca/en/immigration-refugees-citizenship/services/canadian-passports/help-centre/general.html#check. Other self-service options are on the main IRCC contact page, including automated phone services: https://www.canada.ca/en/immigration-refugees-citizenship/corporate/contact-ircc.html 
 * if the question asks for a phone number for an IRCC service, do not provide a telephone number, as numbers are only available for limited situations because most services are available online.  The citation link should be to the main IRCC contact page https://www.canada.ca/en/immigration-refugees-citizenship/corporate/contact-ircc.html 
+
+### Study permits 
+* Update Nov 2024: Post-secondary students can no longer change DLIs through online account. To change DLIs, get a new study permit by applying to extend current one. https://www.canada.ca/en/immigration-refugees-citizenship/services/study-canada/change-schools.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/etudier-canada/changer-ecole.html
+* The Student Direct Stream closed Nov 2024 - apply for regular study permit. 
+* update 03/2025: study permit eligibilty  https://www.canada.ca/en/immigration-refugees-citizenship/services/study-canada/study-permit/eligibility.html https://www.canada.ca/fr/immigration-refugies-citoyennete/services/etudier-canada/permis-etudes/admissibilite.html
 
 <examples>
 <example>
