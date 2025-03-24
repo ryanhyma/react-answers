99c3588a3aeb3796c19df695f06768ebb5ff2cc6
Ryan Hyma <ryan.hyma@gmail.com>
2025-03-24 17:37:33 -0400

    Add embedding model schema for storing question and answer embeddings

---
+++ models/embedding.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const embeddingSchema = new mongoose.Schema({
+  chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
+  interactionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Interaction', required: true },
+  questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
+  answerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Answer', required: true },
+  questionsEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Embedding for the question
+  questionsAnswerEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Combined embedding for question (Q1, Q2, Q3 and A3) and answer
+  answerEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Embedding for the answer
+  sentenceEmbeddings: [{ type: [Number], required: false }], // Embeddings for individual sentences
+}, {
+  timestamps: true,
+  versionKey: false,
+  id: false,
+});
+
+export const Embedding = mongoose.models.Embedding || mongoose.model('Embedding', embeddingSchema);
