76e477593805bc675fe6f93bfade0e38512b9588
Ryan Hyma <ryan.hyma@gmail.com>
2025-03-25 12:58:24 -0400

    Refactor database and model schemas to drop unnecessary indexes and improve clarity in embedding structures

--- models/embedding.js
+++ models/embedding.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
 const embeddingSchema = new mongoose.Schema({
-  chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
+  chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true, },
   interactionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Interaction', required: true },
   questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
   answerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Answer', required: true },
-  questionEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Embedding for the question
-  questionsEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Embedding for questions up to this point Q1, Q2, Q3
-  questionsAnswerEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Combined embedding for question (Q1, Q2, Q3 and A3) and answer
-  answerEmbedding: { type: [Number], required: false, index: true, sparse: true }, // Embedding for the answer
-  sentenceEmbeddings: [{ type: [Number], required: false }], // Embeddings for individual sentences
+  questionEmbedding: { type: [Number], required: false }, // Remove index
+  questionsEmbedding: { type: [Number], required: false }, // Remove index
+  questionsAnswerEmbedding: { type: [Number], required: false }, // Remove index
+  answerEmbedding: { type: [Number], required: false }, // Remove index
+  sentenceEmbeddings: [{ type: [Number], required: false }],
 }, {
   timestamps: true,
   versionKey: false,
